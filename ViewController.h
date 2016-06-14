d3346158c26dcf465d8e4812d164ef8932433c57
CathyMoPro <Cathy@xueluoqingshandeMacBook-Pro.local>
2016-06-14 22:53:41 +0800

    e
    eee

---
+++ ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testQueen
+//
+//  Created by 雪落青山 on 16/6/14.
+//  Copyright © 2016年 Queen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  testQueen
+//
+//  Created by 雪落青山 on 16/6/14.
+//  Copyright © 2016年 Queen. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    NSLog(@"😄");
+
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
